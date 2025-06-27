e7d6edc6222eec49478c03790c2041974d5b9757
Devansh Singh <devansh.20233114@mnnit.ac.in>
2025-06-27 23:21:40 +0530

    Updated the header Section

--- app/page.js
+++ app/page.js
@@ -1,4 +1,8 @@
-import { Button} from "@/components/ui/button";
+import { Button } from "@/components/ui/button";
 export default function Home() {
-  return <Button variant="destructive">Subscribe to RoadsideCoader</Button>;
+  return (
+    <div className="mt-40">
+      <Button variant="destructive">Subscribe to RoadsideCoader</Button>
+    </div>
+  );
 }
