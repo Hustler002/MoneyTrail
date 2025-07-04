8e091cf237ca488efea0406dda1c8cfa60988d33
Devansh Singh <devansh.20233114@mnnit.ac.in>
2025-07-04 11:56:41 +0530

    Created Frontend for transaction table page

--- app/(main)/account/[id]/page.jsx
+++ app/(main)/account/[id]/page.jsx
@@ -13,24 +13,26 @@ const AccountsPage = async ({ params }) => {
 
   const { transactions, ...account } = accountData;
   return (
-    <div className="space-y-8 px-5 flex gap-4 items-end justify-between">
-      <div>
-        <h1 className="text-5xl sm:text-6xl font-bold tracking-tight gradient-title capitalize">
-          {account.name}
-        </h1>
-        <p className="text-muted-foreground">
-          {account.type.charAt(0) + account.type.slice(1).toLowerCase()}
-        </p>
-      </div>
-
-      {/* //Show account balance and transactions */}
-      <div className="text-right pb-2">
-        <div className="text-xl sm:text-2xl font-bold">
-          ${parseFloat(account.balance).toFixed(2)}
+    <div className="space-y-8 px-5">
+      <div className="flex gap-4 items-end justify-between">
+        <div>
+          <h1 className="text-5xl sm:text-6xl font-bold tracking-tight gradient-title capitalize">
+            {account.name}
+          </h1>
+          <p className="text-muted-foreground">
+            {account.type.charAt(0) + account.type.slice(1).toLowerCase()}
+          </p>
         </div>
-        <p className="text-sm text-muted-foreground">
-          {account._count.transactions} Transactions
-        </p>
+
+        {/* //Show account balance and transactions */}
+        <div className="text-right pb-2">
+          <div className="text-xl sm:text-2xl font-bold">
+            ${parseFloat(account.balance).toFixed(2)}
+          </div>
+          <p className="text-sm text-muted-foreground">
+            {account._count.transactions} Transactions
+          </p>
+        </div>
       </div>
 
       {/* Chart Sections */}
@@ -39,7 +41,7 @@ const AccountsPage = async ({ params }) => {
       <Suspense
         fallback={<BarLoader className="mt-4" width={"100%"} color="#9333ea" />}
       >
-        <TransactionTable transactions={transactions}/>
+        <TransactionTable transactions={transactions} />
       </Suspense>
     </div>
   );
