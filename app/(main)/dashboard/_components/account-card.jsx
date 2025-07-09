6bea848960e79127b73b5057db51335ff74797d4
Devansh Singh <devansh.20233114@mnnit.ac.in>
2025-07-09 23:27:16 +0530

    Some Bug Fixes

--- app/(main)/dashboard/_components/account-card.jsx
+++ app/(main)/dashboard/_components/account-card.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useEffect } from "react";
 import {
   Card,
   CardContent,
@@ -13,6 +13,8 @@ import { ArrowUpRight, ArrowDownRight, CreditCard } from "lucide-react";
 import { Switch } from "@/components/ui/switch";
 import { updateDefaultAccount } from "@/actions/account";
 import useFetch from "@/hooks/use-fetch";
+import Link from "next/link";
+import { toast } from "sonner";
 
 const AccountCard = ({ account }) => {
   const { name, type, balance, id, isDefault } = account;
